a5eb3a45d3c84bf2539f9a0e8269cb50d58dfb6b
Vitaly Zorin <addefan@mail.ru>
2024-05-20 17:41:27 +0300

    ! available dates in datetime picker fields in receipt form

--- src/components/forms/receipt-form.jsx
+++ src/components/forms/receipt-form.jsx
@@ -142,9 +142,7 @@ const ReceiptForm = () => {
                         mode="single"
                         selected={field.value}
                         onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                        }
+                        disabled={(date) => date < new Date("1900-01-01")}
                         initialFocus
                       />
                       <div className="p-3 border-t border-border">
@@ -188,9 +186,7 @@ const ReceiptForm = () => {
                         mode="single"
                         selected={field.value}
                         onSelect={field.onChange}
-                        disabled={(date) =>
-                          date > new Date() || date < new Date("1900-01-01")
-                        }
+                        disabled={(date) => date < new Date("1900-01-01")}
                         initialFocus
                       />
                       <div className="p-3 border-t border-border">
